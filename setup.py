d474446ab9d65aacf5076e2d5e7425553dce959c
Isaac Rehg <irehg@ucsd.edu>
2018-03-05 10:54:34 -0800

    add setup.py script to install to conad env with pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='pycocotoolscap',
+      packages=['pycocotoolscap'],
+      package_dir = {'pycocotoolscap': 'pycocotoolscap'},
+      version='1.0'
+      )
+
+setup(name='pycocoevalcap',
+      packages=['pycocoevalcap'],
+      package_dir = {'pycocoevalcap': 'pycocoevalcap'},
+      version='1.0'
+      )
